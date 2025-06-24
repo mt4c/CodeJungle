21da23c23a3a873fbce3cc452a66a75e80d51002
Chengyu Lv (from Dev Box) <chengyulv@microsoft.com>
2025-06-24 11:22:19 +0800

    update fontawesome

--- src/ui/toolbar.js
+++ src/ui/toolbar.js
@@ -1,43 +1,44 @@
-const { library, icon } = require('@fortawesome/fontawesome-svg-core');
-const { faFolderOpen, faFloppyDisk, faPlay } = require('@fortawesome/sharp-solid-svg-icons');
+const { library, icon } = require("@fortawesome/fontawesome-svg-core");
+const {
+  faFolderOpen,
+  faFloppyDisk,
+  faPlay,
+} = require("@fortawesome/free-solid-svg-icons");
 
-library.add(
-    faFolderOpen,
-    faFloppyDisk,
-    faPlay
-);
+// Add icons to the library
+library.add(faFolderOpen, faFloppyDisk, faPlay);
 
 class Toolbar {
-    ele = null
-    buttons = new Map()
+  ele = null;
+  buttons = new Map();
 
-    constructor(toolbarEle) {
-        this.ele = toolbarEle;
-        this.ele.classList.add('jungle-toolbar');
+  constructor(toolbarEle) {
+    this.ele = toolbarEle;
+    this.ele.classList.add("jungle-toolbar");
 
-        const title = document.createElement('div');
-        title.className = 'title';
-        title.innerText = 'CODE JUNGLE';
-        this.ele.appendChild(title);
+    const title = document.createElement("div");
+    title.className = "title";
+    title.innerText = "CODE JUNGLE";
+    this.ele.appendChild(title);
 
-        this.createButton('open', 'folder-open');
-        this.createButton('save', 'floppy-disk');
-        this.createButton('run', 'play');
-    }
+    this.createButton("open", "folder-open");
+    this.createButton("save", "floppy-disk");
+    this.createButton("run", "play");
+  }
 
-    createButton(name, iconName) {
-        const button = document.createElement('div');
-        button.classList.add('button');
-        button.classList.add(name);
+  createButton(name, iconName) {
+    const button = document.createElement("div");
+    button.classList.add("button");
+    button.classList.add(name);
 
-        const iconEle = icon({ prefix: 'fass', iconName });
-        button.innerHTML = iconEle.html;
-        button.setAttribute('title', name);
+    const iconEle = icon({ prefix: "fas", iconName });
+    button.innerHTML = iconEle.html;
+    button.setAttribute("title", name);
 
-        this.ele.appendChild(button);
+    this.ele.appendChild(button);
 
-        this.buttons[name] = button;
-    }
+    this.buttons[name] = button;
+  }
 }
 
 module.exports = { Toolbar };
